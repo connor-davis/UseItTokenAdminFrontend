508599be50135fc1641c9168d453b1fe0e21f47f
Connor <100playermcpe@gmail.com>
2020-11-28 00:13:53 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, {useEffect} from 'react';
+
+import {useDispatch, useSelector} from 'react-redux';
+
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import Auth from './app/pages/auth';
+import Home from './app/pages/home';
+import {selectUser} from './app/slices/user';
+import axios from "axios";
+import {setUsers} from "./app/slices/users";
+import {setItems} from "./app/slices/items";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    let dispatch = useDispatch();
+    let user = useSelector(selectUser);
+
+    useEffect(() => {
+        if (user.secure_secret) {
+            axios.get("http://localhost/users/", {
+                headers: {
+                    "Authorization": "Bearer " + user.token,
+                    "secure_secret": user.secure_secret,
+                }
+            }).then((result) => {
+                if (result.data.error === "no-users-exist") dispatch(setUsers([]));
+                else {
+                    let sorted = result.data.data.sort((a, b) => {
+                        if (a.name > b.name) return 1;
+                        if (a.name < b.name) return -1;
+
+                        return 0;
+                    });
+
+                    let filtered = sorted.filter((filter) => filter.uid !== user.uid);
+
+                    dispatch(setUsers(filtered));
+                }
+
+            }).catch((error) => console.log(error));
+
+            axios.get("http://localhost/items/", {
+                headers: {
+                    "Authorization": "Bearer " + user.token,
+                    "secure_secret": user.secure_secret,
+                }
+            }).then((result) => {
+                if (result.data.error === "no-items-exist") dispatch(setItems([]));
+                else
+                    dispatch(setItems(result.data.data.sort((a, b) => {
+                        if (a.name > b.name) return 1;
+                        if (a.name < b.name) return -1;
+
+                        return 0;
+                    })))
+            }).catch((error) => console.log(error));
+        }
+    });
+
+    return (
+        <Router>
+            {
+                user.uid ?
+                    <Home/> :
+                    <Auth/>
+            }
+        </Router>
+    );
 }
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import {itemsSlice} from "./slices/items";
+import {userSlice} from "./slices/user";
+import {usersSlice} from "./slices/users";
+
+let userReducer = userSlice.reducer;
+let usersReducer = usersSlice.reducer;
+let itemsReducer = itemsSlice.reducer;
+
+function loggerMiddleware(store) {
+    return function (next) {
+        return function (action) {
+            console.log(action)
+            next(action)
+            console.log(store.getState())
+        }
+    }
+}
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({userReducer, usersReducer, itemsReducer})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+let store = configureStore({
+    reducer: persistedReducer,
+    middleware: [...getDefaultMiddleware({
+        serializableCheck: false,
+    }), loggerMiddleware]
+})
+
+let persistor = persistStore(store)
+
+export {store, persistor};
